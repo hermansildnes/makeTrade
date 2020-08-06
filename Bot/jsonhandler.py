6360de5961dd110332575ad5975992e89f17df7f
hermansildnes <herman.sildnes@gmail.com>
2020-08-06 15:18:05 +0200

    Added createJson function to jsonhandler.py

--- Bot/jsonhandler.py
+++ Bot/jsonhandler.py
@@ -7,5 +7,25 @@ def handlejson():
     return var["variables"][0]
 
 
-def createjson(json):
-    pass
+def createjson():
+    data = {}
+    data["variables"] = []
+    data["variables"].append(
+        {
+            "SYMBOL": "BTC/USDT",
+            "DIP_THRESHOLD": -1.00,
+            "UPWARDS_TREND_THRESHOLD": 1.5,
+            "PROFIT_THRESHOLD": 1.25,
+            "STOP_LOSS_THRESHOLD": -2.00,
+            "PERCENT_TO_TRADE": 50,
+            "nextIsBuy": true,
+            "lastOpPrice": 11744.89,
+        }
+    )
+    variables = open("variables.json", "w")
+    json.dump(data, variables)
+    variables.close()
+
+    variable = open("variables.json",)
+    var = json.load(variable)
+    return var["variables"][0]
